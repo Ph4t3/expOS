ffe760b7423eb5f229d723463a9c817223e1d6a5
Ph4t3 <mail@nikhilrajesh.com>
2020-11-17 13:58:57 +0530

    Moved stage 25 assignments to single folder

--- Assignments/Stage25/Assignment_2/run.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-. ../../../load_env.bash
-shopt -s expand_aliases
-
-expl test2.expl
-xi load --exec test2.xsm
-rm test2.xsm
-
-Xsm --debug --timer 1024
---
+++ Assignments/Stage25/Assignments/run.bash
@@ -0,0 +1,151 @@
+#!/bin/bash
+. ../../../load_env.bash
+shopt -s expand_aliases
+xi fdisk
+
+#LIBRARY 
+xi load --library $OS_LAB_PROJ_DIR/myexpos/expl/library.lib
+#OS_STARTUP
+spl os_startup.spl
+xi load --os os_startup.xsm
+rm os_startup.xsm
+
+#MODULES
+spl RESOURCE_MANAGER.spl
+xi load --module 0 RESOURCE_MANAGER.xsm
+rm RESOURCE_MANAGER.xsm
+
+spl PROCESS_MANAGER.spl
+xi load --module 1 PROCESS_MANAGER.xsm
+rm PROCESS_MANAGER.xsm
+
+spl MEMORY_MANAGER.spl
+xi load --module 2 MEMORY_MANAGER.xsm
+rm MEMORY_MANAGER.xsm
+
+spl FILE_MANAGER.spl
+xi load --module 3 FILE_MANAGER.xsm
+rm FILE_MANAGER.xsm
+
+spl DEVICE_MANAGER.spl
+xi load --module 4 DEVICE_MANAGER.xsm
+rm DEVICE_MANAGER.xsm
+
+spl SCHEDULER.spl
+xi load --module 5 SCHEDULER.xsm
+rm SCHEDULER.xsm
+
+spl BOOT_MODULE.spl
+xi load --module 7 BOOT_MODULE.xsm
+rm BOOT_MODULE.xsm
+
+# HARD INTERRUPTS
+spl TIMER_INT.spl
+xi load --int=timer TIMER_INT.xsm
+rm TIMER_INT.xsm
+
+spl DISK_INT.spl
+xi load --int=disk DISK_INT.xsm
+rm DISK_INT.xsm
+
+spl CONSOLE_INT.spl
+xi load --int=console CONSOLE_INT.xsm
+rm CONSOLE_INT.xsm
+
+#SOFT INTERRUPTS
+spl INT_4_FileCD.spl
+xi load --int=4 INT_4_FileCD.xsm
+rm INT_4_FileCD.xsm
+
+spl INT_5_FileOCS.spl
+xi load --int=5 INT_5_FileOCS.xsm
+rm INT_5_FileOCS.xsm
+
+spl INT_6_Read.spl
+xi load --int=6 INT_6_Read.xsm
+rm INT_6_Read.xsm
+
+spl INT_7_Write.spl
+xi load --int=7 INT_7_Write.xsm
+rm INT_7_Write.xsm
+
+spl INT_8_Fork.spl
+xi load --int=8 INT_8_Fork.xsm
+rm INT_8_Fork.xsm
+
+spl INT_9_Exec.spl
+xi load --int=9 INT_9_Exec.xsm
+rm INT_9_Exec.xsm
+
+spl INT_10_Exit.spl
+xi load --int=10 INT_10_Exit.xsm
+rm INT_10_Exit.xsm
+
+spl INT_11.spl
+xi load --int=11 INT_11.xsm
+rm INT_11.xsm
+
+spl INT_13_SemInit.spl
+xi load --int=13 INT_13_SemInit.xsm
+rm INT_13_SemInit.xsm
+
+spl INT_14_SemLock.spl
+xi load --int=14 INT_14_SemLock.xsm
+rm INT_14_SemLock.xsm
+
+spl INT_15_Shutdown.spl
+xi load --int=15 INT_15_Shutdown.xsm
+rm INT_15_Shutdown.xsm
+
+#EXCEPTION HANDLER
+spl EX_HANDLER.spl
+xi load --exhandler EX_HANDLER.xsm
+rm EX_HANDLER.xsm
+
+#IDLE
+expl IDLE.expl 
+xi load --idle IDLE.xsm
+rm IDLE.xsm
+
+#INIT
+expl SHELL.expl
+xi load --init SHELL.xsm
+rm SHELL.xsm
+
+#Other Process 
+expl cat.expl
+xi load --exec cat.xsm
+rm cat.xsm
+
+expl ls.expl
+xi load --exec ls.xsm
+rm ls.xsm
+
+expl rm.expl
+xi load --exec rm.xsm
+rm rm.xsm
+
+expl cp.expl
+xi load --exec cp.xsm
+rm cp.xsm
+
+expl assg1.expl
+xi load --exec assg1.xsm
+rm assg1.xsm
+
+expl assg2.expl
+xi load --exec assg2.xsm
+rm assg2.xsm
+
+expl assg3.expl
+xi load --exec assg3.xsm
+rm assg3.xsm
+
+expl assg4.expl
+xi load --exec assg4.xsm
+rm assg4.xsm
+
+xi load --data sample.dat
+xi load --data numbers.dat
+
+Xsm --debug --timer 1024
